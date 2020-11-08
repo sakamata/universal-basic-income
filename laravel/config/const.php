3cfc417575dbc4dcba5abf08ddf7c4cdb1229cc7
Kazushi Takamura <sakamatapd5@gmail.com>
2020-11-09 02:34:57 +0900

    add User Factory,Seeder

---
+++ laravel/config/const.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'default_point' => 10000,
+];
--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            // todo コメントのconfig化、ロケーション別設定
+            $table->string('name')->comment('ユーザー定義の名前')->comment('未指定は unique_id等の値を代入');
+            $table->string('unique_id', 8)->unique()->comment('自動付与されるid,QRコードやユニークurlに使用');
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->boolean('is_authenticated')->default(0);
+            $table->string('password')->nullable();
             $table->rememberToken();
+            $table->unsignedBigInteger('sharing_after_point')->default(config('const.default_point'))->comment('分配直後の現在ポイント,計算,バックアップ等に利用');
+            $table->string('google_email')->unique()->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
