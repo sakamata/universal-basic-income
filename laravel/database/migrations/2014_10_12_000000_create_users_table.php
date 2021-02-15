aff966e656baf1fe00083230fa12e9b294920b0b
Kazushi Takamura <sakamatapd5@gmail.com>
2021-02-15 20:47:57 +0900

    Edit: DB定義変更 Add: UserSeederにひとまずのFactory 追加

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,13 @@ class CreateUsersTable extends Migration
             $table->id();
             // todo コメントのconfig化、ロケーション別設定
             $table->string('name')->comment('ユーザー定義の名前 未指定は unique_id等の値を代入');
-            $table->string('unique_id', 8)->unique()->comment('自動付与されるid,QRコードやユニークurlに使用');
+            $table->string('unique_id', 16)->unique()->comment('自動付与されるid,QRコードやユニークurlに使用');
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_authenticated')->default(0)->comment('認証済みか、辻ポイントで生成されたユーザーを想定');
             $table->string('password')->nullable();
             $table->rememberToken();
+            $table->unsignedBigInteger('now_point')->default(config('const.default_point'))->comment('現在ポイント');
             $table->unsignedBigInteger('sharing_after_point')->default(config('const.default_point'))->comment('分配直後の現在ポイント,計算,バックアップ等に利用');
             $table->string('google_email')->unique()->nullable()->comment('gmail address');
             $table->timestamps();
--- laravel/database/seeders/UserSeeder.php
+++ laravel/database/seeders/UserSeeder.php
@@ -14,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::factory()->create([
+            'email' => 'aaa@example.com',
+            'unique_id' => 'aaaaaa1234567890',
+            ]);
+        User::factory()->create([
+            'email' => 'bbb@example.com',
+            'unique_id' => 'bbbbbb1234567890',
+            ]);
         User::factory()
                 // ->hasPosts()
                 ->times(50)
